30cd09490c2aefbbb5d4c2e658bf80a48788ef97
jhkot <jhkot@DESKTOP-J7P28PF>
2018-05-14 20:35:50 +0900

    comit

--- src/main/java/com/ljh/white/ledgerRe/service/LedgerReService.java
+++ src/main/java/com/ljh/white/ledgerRe/service/LedgerReService.java
@@ -431,7 +431,7 @@ public class LedgerReService {
 		//데이터 체크			
 		for(int i=0; i<list.size(); i++) {			
 			
-			White.htmlReplace(list.get(i));			
+			White.htmlReplace(list.get(i));
 			White.nullDelete(list.get(i));			
 			
 			record = list.get(i).getString("recordDate");			
