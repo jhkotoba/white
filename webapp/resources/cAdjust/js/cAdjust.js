5e2bd2a7d9d0f457a72fc6bfaaf81597299e4fba
JeHoon <JeHoon@DESKTOP-BCTJDDG>
2018-07-17 22:35:11 +0900

    commit

---
+++ webapp/resources/cAdjust/js/cAdjust.js
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+
+let cAdjust = {
+	
+	data: "",
+	state: "none",		//반복시 상태
+	
+	annCnt: 0,			//개행문자 오기 전까지 주석 개수
+	ann2Idx: 0, 		// /*형식의 주석종료 위치
+	dQuotationIdx: 0, 	// 큰따옴표 종료 위치
+	quotationIdx:0, 	//작은따옴표 종료 위치
+	
+	content : function(){
+		
+	},	
+	
+	_jsp : function(){
+		
+	},
+	_javascript : function(){
+		
+	},
+	_html : function(){
+		
+	},
+	_xml : function(){
+		
+	},
+	_java : function(){
+		
+	}
+}
